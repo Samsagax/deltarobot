ab817ac269e4d60a0c8ab811f3386d37116d7e84
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2012-05-09 18:36:17 -0300

    Created a convenience dsim.h file and add it to Makefile.am

---
+++ dsim/dsim.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2012, Joaquín Ignacio Aramendía
+ * Author: Joaquín Ignacio Aramendía <samsagax [at] gmail [dot] com>
+ *
+ * This file is part of PROJECTNAME.
+ *
+ * PROJECTNAME is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PROJECTNAME is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PROJECTNAME. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * dsim.h :
+ */
+
+#ifndef  DSIM_INC
+#define  DSIM_INC
+
+#include <dsim/dsim_axes.h>
+#include <dsim/dsim_geometry.h>
+#include <dsim/dsim_pos.h>
+#include <dsim/dsim_solver.h>
+#include <dsim/dsim_vector.h>
+
+#endif   /* ----- #ifndef DSIM_INC  ----- */
