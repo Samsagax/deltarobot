674544ee5e2dcf9169246908495717ece71d5eec
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2012-07-17 12:09:35 -0300

    dsim_jacobianh: Split Direct/Inverse jacobian objects

---
+++ dsim/dsim_jacobian.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2012, Joaquín Ignacio Aramendía
+ * Author: Joaquín Ignacio Aramendía <samsagax [at] gmail [dot] com>
+ *
+ * This file is part of PROJECTNAME.
+ *
+ * PROJECTNAME is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PROJECTNAME is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURDIRECT_JACOBIANE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PROJECTNAME. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * dsim_jacobian.h: Objects representing direct/inverse DirectJacobian of the manipulator
+ */
+
+#ifndef  DSIM_DIRECT_JACOBIAN_INC
+#define  DSIM_DIRECT_JACOBIAN_INC
+
+#include <dsim/dsim_matrix.h>
+
+/* Type macros */
+#define D_TYPE_DIRECT_JACOBIAN             (d_direct_jacobian_get_type ())
+#define D_DIRECT_JACOBIAN(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), D_TYPE_DIRECT_JACOBIAN, DDirectJacobian))
+#define D_IS_DIRECT_JACOBIAN(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), D_TYPE_DIRECT_JACOBIAN))
+#define D_DIRECT_JACOBIAN_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), D_TYPE_DIRECT_JACOBIAN, DDirectJacobianClass))
+#define D_IS_DIRECT_JACOBIAN_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), D_TYPE_DIRECT_JACOBIAN))
+#define D_DIRECT_JACOBIAN_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), D_TYPE_DIRECT_JACOBIAN, DDirectJacobianClass))
+
+/* Instance Structure of DDirectJacobian */
+typedef struct _DDirectJacobian DDirectJacobian;
+struct _DDirectJacobian {
+    DMatrix         parent_instance;
+};
+
+/* Class Structure of DDirectJacobian */
+typedef struct _DDirectJacobianClass DDirectJacobianClass;
+struct _DDirectJacobianClass {
+    DMatrixClass    parent_class;
+};
+
+GType       d_direct_jacobian_get_type      (void);
+
+DMatrix*    d_direct_jacobian_new           (DExtAxes    *axes);
+
+/* Type macros */
+#define D_TYPE_INVERSE_JACOBIAN             (d_inverse_jacobian_get_type ())
+#define D_INVERSE_JACOBIAN(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), D_TYPE_INVERSE_JACOBIAN, DInverseJacobian))
+#define D_IS_INVERSE_JACOBIAN(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), D_TYPE_INVERSE_JACOBIAN))
+#define D_INVERSE_JACOBIAN_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), D_TYPE_INVERSE_JACOBIAN, DInverseJacobianClass))
+#define D_IS_INVERSE_JACOBIAN_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), D_TYPE_INVERSE_JACOBIAN))
+#define D_INVERSE_JACOBIAN_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), D_TYPE_INVERSE_JACOBIAN, DInverseJacobianClass))
+
+/* Instance Structure of DInverseJacobian */
+typedef struct _DInverseJacobian DInverseJacobian;
+struct _DInverseJacobian {
+    DMatrix         parent_instance;
+};
+
+/* Class Structure of DInverseJacobian */
+typedef struct _DInverseJacobianClass DInverseJacobianClass;
+struct _DInverseJacobianClass {
+    DMatrixClass    parent_class;
+};
+
+GType       d_inverse_jacobian_get_type     (void);
+
+DMatrix*    d_inverse_jacobian_new          (DExtAxes    *axes);
+#endif   /* ----- #ifndef DSIM_DIRECT_JACOBIAN_INC  ----- */
+
