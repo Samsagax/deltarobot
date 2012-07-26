4f491f17ef34ceb8515a3d5f78940d77098b4b1b
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2012-07-25 21:07:41 -0300

    dsim_manipulator.h: Add abstract object DManipulator.
    DManipulator object represents a Delta robot manipulator and it's
    methods to set motor angles, validations and control.
    
    It can be used as an interface to a real robot or software simulators.

---
+++ dsim/dsim_manipulator.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2012, Joaquín Ignacio Aramendía
+ * Author: Joaquín Ignacio Aramendía <samsagax [at] gmail [dot] com>
+ *
+ * This file is part of PROJECTNAME.
+ *
+ * PROJECTNAME is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PROJECTNAME is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PROJECTNAME. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * dsim_manipulator.h : An object representing a delta robot manipulator
+ * dynamics. Superclass for any real manipulator or dynamic simulator.
+ */
+
+
+#ifndef  DSIM_MANIPULATOR_INC
+#define  DSIM_MANIPULATOR_INC
+
+#include <glib-object.h>
+#include <dsim/dsim_geometry.h>
+#include <dsim/dsim_vector.h>
+#include <dsim/dsim_axes.h>
+
+/* Type macros */
+#define D_TYPE_MANIPULATOR             (d_manipulator_get_type ())
+#define D_MANIPULATOR(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), D_TYPE_MANIPULATOR, DManipulator))
+#define D_IS_MANIPULATOR(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), D_TYPE_MANIPULATOR))
+#define D_MANIPULATOR_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), D_TYPE_MANIPULATOR, DManipulatorClass))
+#define D_IS_MANIPULATOR_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), D_TYPE_MANIPULATOR))
+#define D_MANIPULATOR_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), D_TYPE_MANIPULATOR, DManipulatorClass))
+
+/* Instance Structure of DManipulator */
+typedef struct _DManipulator DManipulator;
+struct _DManipulator {
+    GObject         parent_instance;
+
+    /* Torques in the axes */
+    DVector         *torque;
+
+    /* Axes for the manipulator's motors */
+    DAxes           *axes;
+
+    /* Speed for the manipulator's motors */
+    DSpeed          *speed;
+};
+
+/* Class Structure of DManipulator */
+typedef struct _DManipulatorClass DManipulatorClass;
+struct _DManipulatorClass {
+    GObjectClass    parent_class;
+
+    /* Virtual methods */
+    DVector*    (*get_axes)             (DManipulator *self);
+    void        (*set_axes)             (DManipulator *self, DVector *axes);
+    void        (*set_torque)           (DManipulator *self, DVector *torque);
+    DVector*    (*get_speed)            (DManipulator *self);
+
+    void        (*trans_func)           (DVector *torque, DVector *axes, DVector *speed);
+};
+
+/* Methods */
+GType       d_manipulator_get_type      (void);
+
+
+gdouble     d_manipulator_get_axis      (DManipulator   *self,
+                                         gint           axis);
+
+void        d_manipulator_set_axis      (DManipulator   *self,
+                                         gint           axis,
+                                         gdouble        angle);
+
+DVector*    d_manipulator_get_axes      (DManipulator   *self);
+
+void        d_manipulator_set_axes      (DManipulator   *self,
+                                         DVector        *axes);
+
+DVector*    d_manipulator_get_speed     (DManipulator   *self);
+
+#endif   /* ----- #ifndef DSIM_MANIPULATOR_INC  ----- */
